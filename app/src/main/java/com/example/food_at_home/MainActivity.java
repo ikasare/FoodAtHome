d072a6329de4e7fc28a6f1a25df3669412ad7666
Isaac Asare <67487466+ikasare@users.noreply.github.com>
2022-06-20 16:24:59 -0700

    Home feed created
    Layout for each meal item was made and the recycler view for the whole home feed was made as well. An adapter for the recycler view was made and set to the recycler view in MainActivity. The api was queried for random recipes which was used to populate the home feed. Each recipe's number of likes, servings and time to prepare was gotten and displayed on the home screen as well.

--- app/src/main/java/com/example/food_at_home/MainActivity.java
+++ app/src/main/java/com/example/food_at_home/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.food_at_home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.food_at_home.Adapters.RandomRecipeAdapter;
+import com.example.food_at_home.Listeners.RandomRecipeResponseListener;
+import com.example.food_at_home.Models.RandomRecipeResponse;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ProgressDialog dialog;
+    RequestManager manager;
+    RandomRecipeAdapter adapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Getting Meals...");
+
+        manager = new RequestManager(this);
+        manager.getRandomRecipes(randomRecipeResponseListener);
+        dialog.show();
+
     }
+
+    private final RandomRecipeResponseListener randomRecipeResponseListener = new RandomRecipeResponseListener() {
+        @Override
+        public void fetch(RandomRecipeResponse response, String message) {
+            // dismiss dialog
+            dialog.dismiss();
+            // initialize recycler view
+            recyclerView = findViewById(R.id.rvRandom);
+            // set recycler view layout
+            recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+            adapter = new RandomRecipeAdapter(MainActivity.this, response.recipes);
+            // set adapter onto rv
+            recyclerView.setAdapter(adapter);
+        }
+
+        @Override
+        public void error(String message) {
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
