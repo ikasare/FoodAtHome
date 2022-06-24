3a947841b2dd5ec9b68689ade04de696d5f26151
Isaac Asare <67487466+ikasare@users.noreply.github.com>
2022-06-24 13:22:26 -0700

    Recipe Detail Layout finished and API call made to populate recipe detail layout
    The layout for the recipe detail view was established. It has the food clicked on name, its photo, description and a list of ingredients needed to make that meal in a horizontal recyclerview. The API was then queried to populate data into these views

--- app/src/main/java/com/example/food_at_home/RecipeDetailsActivity.java
+++ app/src/main/java/com/example/food_at_home/RecipeDetailsActivity.java
@@ -1,21 +1,81 @@
 package com.example.food_at_home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.food_at_home.Adapters.IngredientsAdapter;
+import com.example.food_at_home.Listeners.RecipeDetailsListener;
+import com.example.food_at_home.Models.RecipeDetailsResponse;
+import com.squareup.picasso.Picasso;
 
 public class RecipeDetailsActivity extends AppCompatActivity {
 
     int id;
+    TextView tvMealName;
+    TextView tvMealSource;
+    ImageView ivMealImage;
+    TextView tvMealSummary;
+    RecyclerView rvIngredients;
+    RequestManager manager;
+    ProgressDialog dialog;
+    IngredientsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
 
+        initializeViews();
+
         id = Integer.parseInt(getIntent().getStringExtra("id"));
+        manager = new RequestManager(this);
+        manager.getRecipeDetails(listener, id);
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Getting Details...");
+        dialog.show();
 
 
     }
+
+    private void initializeViews() {
+        tvMealName = findViewById(R.id.tvMealName);
+        tvMealSource = findViewById(R.id.tvMealSource);
+        ivMealImage = findViewById(R.id.ivMealImage);
+        tvMealSummary = findViewById(R.id.tvMealSummary);
+        rvIngredients = findViewById(R.id.rvIngredients);
+    }
+
+    private final RecipeDetailsListener listener = new RecipeDetailsListener() {
+        @Override
+        public void fetch(RecipeDetailsResponse response, String message) {
+            dialog.dismiss();
+            String mealName = response.title;
+            tvMealName.setText(mealName);
+            String sourceName = response.sourceName;
+            tvMealSource.setText(sourceName);
+            String summary = response.summary;
+            tvMealSummary.setText(summary);
+            Picasso.get().load(response.image).into(ivMealImage);
+
+            rvIngredients.setHasFixedSize(true);
+            rvIngredients.setLayoutManager(new LinearLayoutManager(RecipeDetailsActivity.this, LinearLayoutManager.HORIZONTAL, false));
+            adapter = new IngredientsAdapter(RecipeDetailsActivity.this, response.extendedIngredients);
+            rvIngredients.setAdapter(adapter);
+
+
+        }
+
+        @Override
+        public void error(String message) {
+            Toast.makeText(RecipeDetailsActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
