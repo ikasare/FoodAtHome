766585d4b870f160c904e04b49291ccb6d861cf5
Isaac Asare <67487466+ikasare@users.noreply.github.com>
2022-07-06 17:01:08 -0700

    Step by Step Guide implemented
    methods created to access api to get the step by step guide to make meals. Also equipments and ingredients associated with these guides have been also displayed where available.

---
+++ app/src/main/java/com/example/food_at_home/Adapters/InstructionStepAdapter.java
@@ -0,0 +1,72 @@
+package com.example.food_at_home.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.food_at_home.Models.Step;
+import com.example.food_at_home.R;
+
+import java.util.List;
+
+public class InstructionStepAdapter extends RecyclerView.Adapter<InstructionStepAdapter.ViewHolder> {
+
+    Context mContext;
+    List<Step> list;
+
+    public InstructionStepAdapter(Context mContext, List<Step> list) {
+        this.mContext = mContext;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.instructions_steps, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String stepNumber = String.valueOf(list.get(position).number);
+        holder.tvInstructionsStepNumber.setText(stepNumber);
+        String step = list.get(position).step;
+        holder.tvInstructionsStepTitle.setText(step);
+
+        holder.rvInstructionIngredients.setHasFixedSize(true);
+        holder.rvInstructionIngredients.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false));
+        InstructionsIngredientsAdapter instructionsIngredientsAdapter = new InstructionsIngredientsAdapter(mContext, list.get(position).ingredients);
+        holder.rvInstructionIngredients.setAdapter(instructionsIngredientsAdapter);
+
+        holder.rvEquipment.setHasFixedSize(true);
+        holder.rvEquipment.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false));
+        InstructionsEquipmentAdapter instructionsEquipmentAdapter = new InstructionsEquipmentAdapter(mContext, list.get(position).equipment);
+        holder.rvEquipment.setAdapter(instructionsEquipmentAdapter);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvInstructionsStepNumber, tvInstructionsStepTitle;
+        RecyclerView rvEquipment, rvInstructionIngredients;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvInstructionsStepNumber = itemView.findViewById(R.id.tvInstructionsStepNumber);
+            tvInstructionsStepTitle = itemView.findViewById(R.id.tvInstructionsStepTitle);
+            rvEquipment = itemView.findViewById(R.id.rvEquipment);
+            rvInstructionIngredients = itemView.findViewById(R.id.rvInstructionIngredients);
+        }
+
+    }
+}
--- app/src/main/java/com/example/food_at_home/RecipeDetailsActivity.java
+++ app/src/main/java/com/example/food_at_home/RecipeDetailsActivity.java
@@ -12,10 +12,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.food_at_home.Adapters.IngredientsAdapter;
+import com.example.food_at_home.Adapters.InstructionStepAdapter;
+import com.example.food_at_home.Adapters.InstructionsAdapter;
 import com.example.food_at_home.Adapters.SimilarRecipeAdapter;
+import com.example.food_at_home.Listeners.InstructionsListener;
 import com.example.food_at_home.Listeners.RecipeClickListener;
 import com.example.food_at_home.Listeners.RecipeDetailsListener;
 import com.example.food_at_home.Listeners.SimilarRecipeListener;
+import com.example.food_at_home.Models.InstructionsResponse;
 import com.example.food_at_home.Models.RecipeDetailsResponse;
 import com.example.food_at_home.Models.SimilarRecipeResponse;
 import com.squareup.picasso.Picasso;
@@ -36,6 +40,7 @@ public class RecipeDetailsActivity extends AppCompatActivity {
     ProgressDialog dialog;
     IngredientsAdapter adapter;
     SimilarRecipeAdapter similarRecipeAdapter;
+    InstructionsAdapter instructionsAdapter;
 
 
     @Override
@@ -49,6 +54,7 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         manager = new RequestManager(this);
         manager.getRecipeDetails(listener, id);
         manager.getSimilarRecipe(similarRecipeListener, id);
+        manager.getInstructions(instructionsListener, id);
         dialog = new ProgressDialog(this);
         dialog.setTitle("Getting Details...");
         dialog.show();
@@ -114,6 +120,21 @@ public class RecipeDetailsActivity extends AppCompatActivity {
             Intent intent = new Intent(RecipeDetailsActivity.this, RecipeDetailsActivity.class);
             intent.putExtra("id", id);
             startActivity(intent);
+
+        }
+    };
+
+    private final InstructionsListener instructionsListener = new InstructionsListener() {
+        @Override
+        public void fetch(List<InstructionsResponse> response, String message) {
+            rvInstructions.setHasFixedSize(true);
+            rvInstructions.setLayoutManager(new LinearLayoutManager(RecipeDetailsActivity.this, LinearLayoutManager.VERTICAL, false));
+            instructionsAdapter = new InstructionsAdapter(RecipeDetailsActivity.this, response);
+            rvInstructions.setAdapter(instructionsAdapter);
+        }
+
+        @Override
+        public void error(String message) {
 
         }
     };
